63e162a23cb87102b56ef3be5aef47273c1a542a
flarn2006 <flarn2006@gmail.com>
2013-09-08 18:29:10 -0400

    Added options for little-endian SET files/overwrite prompt

--- setmerge.c
+++ setmerge.c
@@ -2,12 +2,20 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <errno.h>
+#include <getopt.h>
 
 #define EXIT_SYNTAX 2
 #define EXIT_ERROR  1
 #define EXIT_NORMAL 0
 
+#ifdef DEBUG
+#define DEBUG_PRINTF printf
+#else
+#define DEBUG_PRINTF(...)
+#endif
+
 typedef uint8_t SETEntry[32];
+typedef enum { SET_LITTLE_ENDIAN, SET_BIG_ENDIAN } endianness_t;
 
 void swapBytes(uint8_t *byte1, uint8_t *byte2)
 {
@@ -16,28 +24,32 @@ void swapBytes(uint8_t *byte1, uint8_t *byte2)
 	*byte2 = temp;
 }
 
-uint32_t readSETHeader(FILE *fp)
+uint32_t readSETHeader(FILE *fp, endianness_t endianness)
 {
 	// Returns number of objects.
 	uint32_t objects;
 	fread(&objects, 4, 1, fp);
 	fseek(fp, 32, SEEK_SET);
 
-	// Convert to little endian
-	uint8_t *ptr = (uint8_t*)&objects;
-	swapBytes(ptr, ptr + 3);
-	swapBytes(ptr + 1, ptr + 2);
+	if (endianness == SET_BIG_ENDIAN) {
+		// Convert to little endian
+		uint8_t *ptr = (uint8_t*)&objects;
+		swapBytes(ptr, ptr + 3);
+		swapBytes(ptr + 1, ptr + 2);
+	}
 	
 	// Return 0 if EOF already.
 	return feof(fp) ? 0 : objects;
 }
 
-void writeSETHeader(FILE *fp, uint32_t num_objects)
+void writeSETHeader(FILE *fp, uint32_t num_objects, endianness_t endianness)
 {
-	// Convert to big endian
-	uint8_t *ptr = (uint8_t*)&num_objects;
-	swapBytes(ptr, ptr + 3);
-	swapBytes(ptr + 1, ptr + 2);
+	if (endianness == SET_BIG_ENDIAN) {
+		// Convert to big endian
+		uint8_t *ptr = (uint8_t*)&num_objects;
+		swapBytes(ptr, ptr + 3);
+		swapBytes(ptr + 1, ptr + 2);
+	}
 
 	fwrite(&num_objects, 4, 1, fp);
 
@@ -64,19 +76,35 @@ int doesFileExist(const char *filename)
 
 int main(int argc, char *argv[])
 {
-	if (argc < 4) {
-		printf("Usage: %s set1 set2 [...] output\n", argv[0]);
+	int opt;
+	endianness_t endianness = SET_BIG_ENDIAN;
+	int prompt_to_overwrite = 1;
+
+	while ((opt = getopt(argc, argv, "ef")) != -1) {
+		switch (opt) {
+			case 'e': endianness = SET_LITTLE_ENDIAN; break;
+			case 'f': prompt_to_overwrite = 0; break;
+		}
+	}
+
+	DEBUG_PRINTF("argc=%d, optind=%d\n", argc, optind);
+
+	if (argc - optind < 3) {
+		printf("Usage: %s [-ef] set1 set2 ... output\n", argv[0]);
+		printf(" -e\tTreat SET files as little-endian\n");
+		printf(" -f\tOverwrite files without prompting\n");
 		return EXIT_SYNTAX;
 	}
 
-	int num_files = argc - 2;
+	int num_files = argc - 1 - optind;
 	uint32_t *object_counts = malloc(4 * num_files);
 	uint32_t total_objects = 0;
 	SETEntry **data = malloc(sizeof(SETEntry*) * num_files);
 
 	int i; for (i=0; i<num_files; i++) {
 		// loop through the input filenames
-		const char *filename = argv[i+1];
+		const char *filename = argv[i+optind];
+		DEBUG_PRINTF("Input file:  %s\n", filename);
 		FILE *fp = fopen(filename, "rb");
 
 		if (!fp) {
@@ -85,10 +113,16 @@ int main(int argc, char *argv[])
 			return EXIT_ERROR;
 		}
 
-		uint32_t num_objects = readSETHeader(fp);
+		uint32_t num_objects = readSETHeader(fp, endianness);
 		object_counts[i] = num_objects;
 		total_objects += num_objects;
 		data[i] = malloc(sizeof(SETEntry) * num_objects);
+
+		if (data[i] == NULL) {
+			printf("Not enough memory! Are you sure those are all SET files?\n");
+			printf("(Make sure you have the endianness correct, and use -e if necessary.)\n");
+			return EXIT_ERROR;
+		}
 
 		fread(data[i], sizeof(SETEntry), num_objects, fp);
 
@@ -102,7 +136,9 @@ int main(int argc, char *argv[])
 	}
 
 	const char *out_filename = argv[argc-1];
+	DEBUG_PRINTF("Output file: %s\n", out_filename);
 
+	if (prompt_to_overwrite) // Don't check whether the file exists if not necessary
 	if (doesFileExist(out_filename)) {
 		printf("Output file %s already exists!\n", out_filename);
 		printf("Do you want to overwrite it? [no]: ");
@@ -122,7 +158,7 @@ int main(int argc, char *argv[])
 		return EXIT_ERROR;
 	}
 
-	writeSETHeader(fp, total_objects);
+	writeSETHeader(fp, total_objects, endianness);
 
 	for (i=0; i<num_files; i++) {
 		uint32_t num_objects = object_counts[i];
